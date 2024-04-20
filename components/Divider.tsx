3fee9a52d0d21f89a22a271d807692fed8e578f9
Vidhanth <vidhanthbafna@gmail.com>
2024-04-20 13:42:59 +0530

    Animate dividers

--- components/Divider.tsx
+++ components/Divider.tsx
@@ -1,15 +1,19 @@
+import FadeAnimation from "./FadeAnimation";
+
 type DividerProps = {
   visible?: boolean;
 };
 
-export default function Divider({
-  visible = true,
-}: DividerProps) {
+export default function Divider({ visible = true }: DividerProps) {
   return (
-    <div
-      className={`${
-        visible ? "bg-gradient-to-r to-primary from-tertiary my-10" : "my-5"
-      } w-full h-px`}
-    ></div>
+    <FadeAnimation overrideDirection="up" animateOnVisibility>
+      <div
+        className={`${
+          visible
+            ? "bg-gradient-to-r to-primary from-tertiary my-8 md:my-10"
+            : "my-5"
+        } w-full h-px`}
+      ></div>
+    </FadeAnimation>
   );
 }
