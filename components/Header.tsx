e28d439cbb8915f13f485840c9cad2fa10e19c26
Vidhanth <vidhanthbafna@gmail.com>
2024-04-20 13:43:18 +0530

    Fix header sizes on smaller screens

--- components/Header.tsx
+++ components/Header.tsx
@@ -21,20 +21,24 @@ export default function Header({
     <FadeIn>
       <div className={`bg-${bgTexture} ${className}`}>
         <div
-          className={`${
+          className={`px-6 ${
             bgTexture?.length || 0 > 0
-              ? "bg-black bg-opacity-50 text-white py-16 px-6"
+              ? "bg-black bg-opacity-50 text-white py-16"
               : ""
           }`}
         >
-          <div className="max-w-6xl text-center mx-auto p-0 flex flex-col items-center justify-center gap-y-4">
+          <div className="max-w-6xl text-center mx-auto p-0 flex flex-col items-center justify-center gap-y-2 md:gap-y-4">
             {preheader && (
               <FadeInWhenVisible delay={0.1}>
                 <p className="font-bold">{preheader}</p>
               </FadeInWhenVisible>
             )}
             <FadeInUpWhenVisible delay={0.3}>
-              <h1 className={archivo.className + " text-5xl font-bold"}>
+              <h1
+                className={
+                  archivo.className + " text-3xl md:text-5xl font-bold"
+                }
+              >
                 {title}
               </h1>
             </FadeInUpWhenVisible>
