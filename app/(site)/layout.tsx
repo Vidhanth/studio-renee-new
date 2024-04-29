afdbc8a81e110cebfc6a58a77d807e1f031a2ed4
Vidhanth <vidhanthbafna@gmail.com>
2024-04-29 23:13:04 +0530

    Add whatsapp chat button

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -5,6 +5,7 @@ import { FadeIn } from "@/transitions";
 import "../globals.css";
 import { Metadata } from "next";
 import { defaultMetadata } from "@/constants";
+import { FaWhatsapp } from "react-icons/fa6";
 
 export const metadata: Metadata = defaultMetadata;
 
@@ -21,6 +22,16 @@ export default function RootLayout({
         </header>
         <main>
           <FadeIn>{children}</FadeIn>
+          <FadeIn delay={2}>
+            <a
+              href="//wa.me/+919901897130?text=Hi"
+              target="_blank"
+              rel="noreferrer"
+              className="bg-green-600 fixed bottom-4 right-4 md:bottom-8 md:right-8 p-2.5 rounded-full shadow-md shadow-dark-300 cursor-pointer"
+            >
+              <FaWhatsapp className=" text-white text-3xl" />
+            </a>
+          </FadeIn>
         </main>
         <footer>
           <PreFooterSection />
